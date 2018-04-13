3ad1ed7253743278fc0b18d5131a3f626408960f
Ernesto Ulate <netou18@gmail.com>
2018-04-13 11:37:24 -0600

    Synthetize bug to fix RegPC.sv, but in this moment seems to work.

--- cpu_pipelined/hdl/RegPC.sv
+++ cpu_pipelined/hdl/RegPC.sv
@@ -10,13 +10,21 @@ module RegPC #(
 
 logic [SIZE-1:0] PCF_tmp;
 
-always@( negedge CLK )
+always@(RESET)
 begin
-		if( RESET ) PCF_tmp <= {SIZE{1'bx}};
-		else PCF_tmp <= {SIZE{1'b0}};
-     if ( ~StallF )
+    PCF_tmp <= {SIZE{1'bx}};
+end
+
+always@(~RESET)
+begin
+    PCF_tmp <= {SIZE{1'b0}};
+end
+
+always@(negedge CLK & ~RESET)
+begin
+     if (StallF == 1'b0)
         PCF_tmp <= PC;
-    else if ( StallF == 1'b1 )
+    else if ((StallF == 1'b1))
         PCF_tmp <= PCF_tmp;
     else
         PCF_tmp <= PC;
@@ -25,3 +33,32 @@ end
 assign PCF = PCF_tmp;
 
 endmodule
+
+/**`timescale 1ns / 1ps
+
+module RegPC #(
+    parameter SIZE = 32
+)(
+    input logic StallF, CLK, RESET,
+    input logic [SIZE-1:0] PC,
+    output logic [SIZE-1:0] PCF
+);
+
+logic [SIZE-1:0] PCF_tmp;
+
+always@(negedge CLK & RESET)
+begin
+    //if (RESET)
+    //    PCF_tmp <= {SIZE{1'bx}};
+    //if (~RESET)
+    //    PCF_tmp <= {SIZE{1'b0}};
+
+    if (StallF == 1'b0)
+        PCF_tmp <= PC;
+    else if (StallF == 1'b1)
+        PCF_tmp <= PCF_tmp;
+end
+
+assign PCF = PCF_tmp;
+
+endmodule */
